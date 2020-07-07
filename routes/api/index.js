b716ec36d1807687b3be993dd780796834acd0d4
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-07-06 21:54:15 -0700

    updating routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
+// Import all of the API routes from /api/index.js (no need for index.js though since it's implied)
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html/html-routes');
 
-//add prefix of `/pizzas` to routes created in `pizza-routes.js`
-router.use('/pizzas', pizzaRoutes);
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
 
 module.exports = router;
